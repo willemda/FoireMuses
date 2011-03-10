dbcc0396ca20273210730fe2015bfa6bac7a2891
Vincent DARON <vdaron@ask.be>
2011-03-10 13:31:44 +0100

    Add Context and Instance classes

---
+++ Core/Context.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MindTouch.Dream;
+
+namespace Core
+{
+	public class Context
+	{
+		public static Context Current
+		{
+			get
+			{
+				Context dc = CurrentOrNull;
+				if (dc == null)
+				{
+					throw new InvalidOperationException("A247Context.Current is not set");
+				}
+				return dc;
+			}
+		}
+		public static Context CurrentOrNull
+		{
+			get
+			{
+				DreamContext dreamContext = DreamContext.Current;
+				var context = dreamContext.GetState<Context>();
+				if (context == null)
+				{
+					throw new Exception("Invalid context");
+				}
+				return context;
+			}
+		}
+
+		//TODO : Specify a Type for the user.
+		public object User { get; set; }
+		public Instance Instance { get; private set; }
+
+		public Context(Instance anInstance)
+		{
+			Instance = anInstance;
+		}
+	}
+}
---
+++ Core/Instance.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core
+{
+	/// <summary>
+	/// Contains a reference to the Controllers
+	/// </summary>
+	public class Instance
+	{
+		
+	}
+}
