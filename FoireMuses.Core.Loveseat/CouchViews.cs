124dc3a2c58e82b74bc3d139b208d8eaf2bcb585
Vincent DARON <vdaron@ask.be>
2011-03-28 08:41:02 +0200

    Fix formatting

--- FoireMuses.Core.Loveseat/CouchViews.cs
+++ FoireMuses.Core.Loveseat/CouchViews.cs
@@ -5,19 +5,18 @@ using System.Text;
 
 namespace FoireMuses.Core.Utils
 {
-    public class CouchViews
-    {
-        public const string VIEW_SOURCES = "sources";
-        public const string VIEW_PLAYS = "plays";
-        public const string VIEW_SCORES = "scores";
-        public const string VIEW_USERS = "users";
+	public class CouchViews
+	{
+		public const string VIEW_SOURCES = "sources";
+		public const string VIEW_PLAYS = "plays";
+		public const string VIEW_SCORES = "scores";
+		public const string VIEW_USERS = "users";
 
-        public const string VIEW_ALL = "all";
+		public const string VIEW_ALL = "all";
 
-        public const string VIEW_SCORES_FROM_SOURCE = "fromsource";
-        public const string VIEW_SCORES_FROM_PLAY = "fromplay";
+		public const string VIEW_SCORES_FROM_SOURCE = "fromsource";
+		public const string VIEW_SCORES_FROM_PLAY = "fromplay";
 
-
-        public const string VIEW_USERS_BY_USERNAME = "byusername";
-    }
+		public const string VIEW_USERS_BY_USERNAME = "byusername";
+	}
 }
